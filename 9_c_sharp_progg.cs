adcea7f58937bec82f18616e1ad5c24bec179f0d
Alexandre Pereira <33624800+Alexandremp@users.noreply.github.com>
2020-12-28 17:11:23 +0000

    Create 9_c_sharp_progg.cs

---
+++ 9_c_sharp_progg.cs
@@ -0,0 +1,10 @@
+using System;
+
+// A version of the classic "Hello World" program
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, world!");
+    }
+}
